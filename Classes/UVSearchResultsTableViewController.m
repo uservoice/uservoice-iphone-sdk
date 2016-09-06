0132bbddf960d65c2ca42e057265a0d4ab947bb2
Donny Davis <donnywdavis@icloud.com>
2016-09-06 11:26:36 -0400

    Fix issue where only one search result was showing.

--- Classes/UVSearchResultsTableViewController.m
+++ Classes/UVSearchResultsTableViewController.m
@@ -9,10 +9,12 @@
 #import "UVSearchResultsTableViewController.h"
 #import "UVArticle.h"
 #import "UVBaseViewController.h"
+#import "UVTruncatingLabel.m"
 
 @interface UVSearchResultsTableViewController ()
 
 @property (nonatomic, strong) UVBaseViewController *baseVC;
+@property (nonatomic, retain) NSMutableDictionary *templateCells;
 
 @end
 
@@ -21,6 +23,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    _templateCells = [NSMutableDictionary dictionary];
     _baseVC = [[UVBaseViewController alloc] init];
 }
 
@@ -96,12 +99,30 @@
     if ([self respondsToSelector:customizeCellSelector]) {
         [self performSelector:customizeCellSelector withObject:cell withObject:indexPath];
     }
+    if (!IOS7) {
+        cell.contentView.frame = CGRectMake(0, 0, [self cellWidthForStyle:tableView.style accessoryType:cell.accessoryType], 0);
+        [cell.contentView setNeedsLayout];
+        [cell.contentView layoutIfNeeded];
+        for (UIView *view in cell.contentView.subviews) {
+            if ([view isKindOfClass:[UILabel class]]) {
+                UILabel *label = (UILabel *)view;
+                if (label.numberOfLines != 1) {
+                    [label setPreferredMaxLayoutWidth:label.frame.size.width];
+                }
+                [label setBackgroundColor:[UIColor clearColor]];
+            } else if ([view isKindOfClass:[UVTruncatingLabel class]]) {
+                UVTruncatingLabel *label = (UVTruncatingLabel *)view;
+                [label setPreferredMaxLayoutWidth:label.frame.size.width];
+            }
+        }
+    }
     
     return cell;
 //    return [_baseVC createCellForIdentifier:identifier tableView:tableView indexPath:indexPath style:style selectable:YES];
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
     [_instantAnswerManager pushViewFor:[_searchResults objectAtIndex:indexPath.row] parent:self];
 }
 
@@ -113,7 +134,82 @@
     } else {
         identifier = @"SuggestionResult";
     }
-    return [_baseVC heightForDynamicRowWithReuseIdentifier:identifier indexPath:indexPath];
+    return [self heightForDynamicRowWithReuseIdentifier:identifier indexPath:indexPath];
+}
+
+- (CGFloat)heightForDynamicRowWithReuseIdentifier:(NSString *)reuseIdentifier indexPath:(NSIndexPath *)indexPath {
+    NSString *cacheKey = [NSString stringWithFormat:@"%@-%d", reuseIdentifier, (int)self.view.frame.size.width];
+    UITableViewCell *cell = [_templateCells objectForKey:cacheKey];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:0 reuseIdentifier:reuseIdentifier];
+        SEL initCellSelector = NSSelectorFromString([NSString stringWithFormat:@"initCellFor%@:indexPath:", reuseIdentifier]);
+        if ([self respondsToSelector:initCellSelector]) {
+            [self performSelector:initCellSelector withObject:cell withObject:nil];
+        }
+        [_templateCells setObject:cell forKey:cacheKey];
+    }
+    SEL customizeCellSelector = NSSelectorFromString([NSString stringWithFormat:@"customizeCellFor%@:indexPath:", reuseIdentifier]);
+    if ([self respondsToSelector:customizeCellSelector]) {
+        [self performSelector:customizeCellSelector withObject:cell withObject:indexPath];
+    }
+    cell.contentView.frame = CGRectMake(0, 0, [self cellWidthForStyle:self.tableView.style accessoryType:cell.accessoryType], 10000);
+    [cell.contentView setNeedsLayout];
+    [cell.contentView layoutIfNeeded];
+    
+    // cells are usually flat so I don't bother to iterate recursively
+    for (UIView *view in cell.contentView.subviews) {
+        if ([view isKindOfClass:[UILabel class]]) {
+            UILabel *label = (UILabel *)view;
+            if (label.numberOfLines != 1) {
+                [label setPreferredMaxLayoutWidth:label.frame.size.width];
+            }
+        }
+    }
+    [cell.contentView setNeedsLayout];
+    [cell.contentView layoutIfNeeded];
+    
+    return [cell.contentView systemLayoutSizeFittingSize:UILayoutFittingCompressedSize].height + 1;
+}
+
+- (CGFloat)cellWidthForStyle:(UITableViewStyle)style accessoryType:(UITableViewCellAccessoryType)accessoryType {
+    CGFloat width = self.view.frame.size.width;
+    CGFloat accessoryWidth = 0;
+    CGFloat margin = 0;
+    if (IOS8) {
+        if (IPAD || IPHONE_PLUS) {
+            if (accessoryType == UITableViewCellAccessoryDisclosureIndicator) {
+                accessoryWidth = 38;
+            } else if (accessoryType == UITableViewCellAccessoryCheckmark) {
+                accessoryWidth = 44;
+            }
+        } else {
+            if (accessoryType == UITableViewCellAccessoryDisclosureIndicator) {
+                accessoryWidth = 34;
+            } else if (accessoryType == UITableViewCellAccessoryCheckmark) {
+                accessoryWidth = 40;
+            }
+        }
+    } else if (IOS7) {
+        if (accessoryType == UITableViewCellAccessoryDisclosureIndicator) {
+            accessoryWidth = 33;
+        } else if (accessoryType == UITableViewCellAccessoryCheckmark) {
+            accessoryWidth = 38.5;
+        }
+    } else {
+        if (accessoryType == UITableViewCellAccessoryDisclosureIndicator || accessoryType == UITableViewCellAccessoryCheckmark) {
+            accessoryWidth = 20;
+        }
+        if (width > 20) {
+            if (width < 400) {
+                margin = 10;
+            } else {
+                margin = MAX(31, MIN(45, width*0.06f));
+            }
+        } else {
+            margin = width - 10;
+        }
+    }
+    return width - (style == UITableViewStyleGrouped ? margin * 2 : 0) - accessoryWidth;
 }
 
 @end
